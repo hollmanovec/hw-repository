4b8f61e07dba0b81884d77d511d6963db741a8d0
BuildTools <unconfigured@null.spigotmc.org>
2024-03-26 17:11:07 +0100

    main in Folder 2 updated

--- Folder 2/main2.py
+++ Folder 2/main2.py
@@ -1,2 +1,19 @@
-#This file is about to be changed in the next task
-print("*" * 5)
+#Write a function that takes two numbers as a parameter and displays all odd numbers inbetween
+
+num1 = int(input("zadejte první číslo:"))
+num2 = int(input("zadejte druhé číslo:"))
+
+def odd_numbers():
+  for i in range(num1, num2):
+    if i % 2 == 1:
+      print(i, end=" ")
+
+odd_numbers()
+
+
+def odd_numbers2(start=0, end =10):    #0 a 10 předdefinováno, můžeme to přebít
+  for i in range(start, end):          # end a start může být i prohozené v tom definování,
+    if i % 2 == 1:                     #protože samotná funkce to má správně
+      print(i, end=" ")
+
+odd_numbers2()
